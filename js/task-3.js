e4195d549687112d87cc574e1106b40b9a99a2df
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2024-11-12 14:56:52 +0300

    task-3 is written.

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,18 @@
-function checkForSpam(message){
-  let isSpam=message.includes("spam");
-  let isSpam=message.includes("sale");
-  let isSpam=message.includes("SalE");
-  let isSpam=message.includes("[SPAM]");
-  if (isSpam){
-    console.log(true);
-
-  }else{
-    console.log(false);
+"use strict";
+function checkForSpam(message) {
+  const checkSpam = message.toLowerCase().includes("spam");
+  const checkSale = message.toLowerCase().includes("sale");
+  if (checkSale||checkSpam) {
+    return true;
+  } else {
+    return false;
   }
-return
 }
-checkForSpam("Latest technology news"); // false
-checkForSpam("JavaScript weekly newsletter"); // false
-checkForSpam("Get best sale offers now!"); // true
-checkForSpam("Amazing SalE, only tonight!"); // true
-checkForSpam("Trust me, this is not a spam message"); // true
-checkForSpam("Get rid of sPaM emails. Our book in on sale!"); // true
-checkForSpam("[SPAM] How to earn fast money?"); // true
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
