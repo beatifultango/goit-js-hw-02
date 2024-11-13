fcf9f365ad122dd47e4b07a6e54758dd9d919f89
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2024-11-13 13:16:25 +0300

    düzeltmeler tamamlandı.

--- js/task-1.js
+++ js/task-1.js
@@ -5,7 +5,7 @@ function makeTransaction(quantity,pricePerDroid,customerCredits){
   } else{
     console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`)
   }
-  return totalPrice
+  return
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,30 @@
 function getShippingCost(country) {
+  let cost;
   switch (country) {
     case "China":
-      console.log(`Shipping to ${country} will cost 100 credits`);
+      cost = 100;
+      console.log(`Shipping to ${country} will cost ${cost}  credits`);
       break;
     case "Chile":
-      console.log(`Shipping to ${country} will cost 250 credits`);
+      cost = 250;
+      console.log(`Shipping to ${country} will cost ${cost}  credits`);
       break;
     case "Australia":
-      console.log(`Shipping to ${country} will cost 170 credits`);
+      cost = 170;
+      console.log(`Shipping to ${country} will cost ${cost}  credits`);
       break;
     case "Jamaica":
-      console.log(`Shipping to ${country} will cost 120 credits`);
+      cost = 120;
+      console.log(`Shipping to ${country} will cost ${cost}  credits`);
       break;
+
     default:
       console.log("Sorry, there is no delivery to your country");
   }
 }
-getShippingCost("Australia"); // "Shipping to Australia will cost 170 credits"
-getShippingCost("Germany"); // "Sorry, there is no delivery to your country"
-getShippingCost("China"); // "Shipping to China will cost 100 credits"
-getShippingCost("Chile"); // "Shipping to Chile will cost 250 credits"
-getShippingCost("Jamaica"); // "Shipping to Jamaica will cost 120 credits"
-getShippingCost("Sweden"); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
